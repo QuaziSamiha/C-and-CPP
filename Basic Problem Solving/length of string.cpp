cce5befd808d3bef04a841136293914fe982062c
QuaziSamiha <1703155@student.ruet.ac.bd>
2024-02-13 10:35:45 +0600

    string length and reverse done

---
+++ Basic Problem Solving/length of string.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    char str[100]= "Samiha Tasnim";
+
+    int i = 0, length = 0;
+    while (str[i] != '\0') // till null character
+    {
+        i++;
+        length++;
+    }
+    cout << "Length of String: " << length << endl;
+}
