5b32aff9d781ee8c3051fabc2a16d948e48c200f
QuaziSamiha <1703155@student.ruet.ac.bd>
2024-02-14 23:34:41 +0600

    easy pattern done

---
+++ Basic Problem Solving/patterns/pattern1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i, j;
+
+    for (i = 0; i < 5; i++)
+    {
+        for (j = 1; j <= 5; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
---
+++ Basic Problem Solving/patterns/pattern3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i, j;
+
+    for (j = 1; j <= 5; j++)
+    {
+        cout << "A B C D E" << endl;
+    }
+}
