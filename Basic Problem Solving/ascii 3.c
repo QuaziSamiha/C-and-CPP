98fc0feeb7a755b01d94baf12952dd2f52ee20ce
QuaziSamiha <1703155@student.ruet.ac.bd>
2024-02-15 09:01:36 +0600

    ASCII value done

---
+++ Basic Problem Solving/ascii 3.c
@@ -0,0 +1,32 @@
+// convert Character to ASCII value
+// 15.02.24
+#include<stdio.h>
+
+int main(){
+    char ch = 'A';
+    printf("ASCII Value %d", ch);
+    return 0;
+}
+
+
+// capital A = ascii value 65
+// small a = ascii value 97
+
+/*
+ASCII stands for American Standard Code for Information Interchange. It's a character encoding standard 
+for electronic communication. In ASCII, each character is represented by a 7-bit binary number (0 to 127), 
+which corresponds to a particular symbol or character. For example, the ASCII value for the letter 'A' is 65, 'B' is 66, and so on.
+However, with extended ASCII, which uses 8 bits (0 to 255), more characters can be represented, including 
+special characters, symbols, and accented letters.
+
+Here's a basic example of ASCII values for some common characters:
+
+- 'A' has an ASCII value of 65
+- 'B' has an ASCII value of 66
+- 'a' has an ASCII value of 97
+- '1' has an ASCII value of 49
+- Space has an ASCII value of 32
+- The newline character ('\n') has an ASCII value of 10
+
+These values are used by computers to represent characters internally and in data transmission.
+*/
--- Basic Problem Solving/patterns/practice.cpp
+++ Basic Problem Solving/patterns/practice.cpp
@@ -8,7 +8,7 @@ int main()
 
     for (i = 5; i >= 1; i--)
     {
-        for (j = 5; j >= i; j--)
+        for (j = 5; j <= i; j--)
         {
             cout << j << " ";
         }
