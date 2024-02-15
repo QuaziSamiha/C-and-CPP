c578376b98780696bbd0fb67ecbc996eaa530fc6
QuaziSamiha <1703155@student.ruet.ac.bd>
2024-02-15 10:28:39 +0600

    decimal octal hexadecimal conversion done

---
+++ Basic Problem Solving/base1.c
@@ -0,0 +1,16 @@
+// conversion between decimal to octal
+// 15.02.24
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("\nDecimal Number  : ");
+    scanf("%d", &n);
+    printf("Octal Number: %o", n);
+
+    printf("\n\nOctal Number  : ");
+    scanf("%o", &n);
+    printf("Decimal Number: %d", n);
+    return 0;
+}
--- Basic Problem Solving/base2.c
+++ Basic Problem Solving/base2.c
@@ -1,16 +1,16 @@
-// conversion between octal and hexadecimal
+// conversion between decimal to Hexa-decimal
 // 15.02.24
 #include <stdio.h>
 
 int main()
 {
     int n;
-    printf("\nOctal Number  : ");
-    scanf("%o", &n);
+    printf("\nDecimal Number  : ");
+    scanf("%d", &n);
     printf("Hexa-decimal Number: %x", n);
 
     printf("\n\nHexa-decimal Number  : ");
     scanf("%x", &n);
-    printf("Octal Number: %o", n);
+    printf("Decimal Number: %d", n);
     return 0;
 }
