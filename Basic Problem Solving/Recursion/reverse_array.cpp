872aa2def80b09da7511e093e7dd5aae2f9e4c87
QuaziSamiha <1703155@student.ruet.ac.bd>
2024-03-12 23:54:10 +0600

    reverse array with recursion

---
+++ Basic Problem Solving/Recursion/reverse_array.cpp
@@ -0,0 +1,27 @@
+/* Online C++ Compiler and Editor */
+#include <iostream>
+
+using namespace std;
+
+void traverse_array(int a[], int f, int l);
+
+int main()
+{
+    int a[] = {5, 7, 9, 11, 6};
+    traverse_array(a, 0, 4);
+    return 0;
+}
+
+void traverse_array(int a[], int f, int l)
+{
+    if (f == l)
+    {
+        cout << a[f] << "\t";
+        return;
+    }
+    else
+    {
+        traverse_array(a, f + 1, l);
+        cout << a[f] << "\t";
+    }
+}
