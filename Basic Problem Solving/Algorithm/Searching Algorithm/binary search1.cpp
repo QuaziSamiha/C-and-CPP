c3951b4367bdd408b3a595958387bfaf433a2321
QuaziSamiha <1703155@student.ruet.ac.bd>
2025-07-09 13:30:16 +0600

    preparing for welldev

--- Basic Problem Solving/Algorithm/Searching Algorithm/binary search1.cpp
+++ Basic Problem Solving/Algorithm/Searching Algorithm/binary search1.cpp
@@ -40,6 +40,7 @@ int main(void)
 
 /*
 02.04.24
+09.07.25
 Binary Search is defined as a searching algorithm used in a sorted array by repeatedly dividing
 the search interval in half. The idea of binary search is to use the
 information that the array is sorted and reduce the time complexity to O(log N).
